c1aebb166686e59b8c2be9ec55cfd4497c3b46fb
ed <edwardt.tril@gmail.com>
2012-09-09 15:13:38 -0700

    deploy package

---
+++ ASPMVC3/TraceThroughASPMVC3/MvcApplication1/MvcApplication1/Areas/Usage/UsageAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MvcApplication1.Areas.Usage
+{
+    public class UsageAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Usage";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Usage_default",
+                "Usage/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
--- OOPAccessModifier/OOPAccessModifier/OOPAccessModifier/Program.cs
+++ OOPAccessModifier/OOPAccessModifier/OOPAccessModifier/Program.cs
@@ -7,6 +7,14 @@ namespace OOPAccessModifier
 {
     class Program
     {
+        //public, protected, private, default
+        //protected internal, public intern, private internal
+        //on class, properties, interface, methods, abstract class, abstract method
+        //overide, virtual, new
+        //static,readonly, const effect
+        //Thread?, app domain? thread local
+
+
         static void Main(string[] args)
         {
         }
