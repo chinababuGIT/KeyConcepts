c1f88475a2a4a579550c02535e3a141a5f7ff28b
ed <edwardt.tril@gmail.com>
2012-11-14 22:47:08 -0800

    new package

--- Threading/ThreadingBasics/ThreadingBasics/Program.cs
+++ Threading/ThreadingBasics/ThreadingBasics/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Threading;
 
 namespace ThreadingBasics
 {
@@ -10,9 +10,28 @@ namespace ThreadingBasics
     {
         static void Main(string[] args)
         {
-            BaiscThreadingOperation basicthread= new BaiscThreadingOperation(5);
-            basicthread.Run();
-            basicthread.WaitForThreads();
+            //BaiscThreadingOperation basicthread= new BaiscThreadingOperation(5);
+            //basicthread.Run();
+            //basicthread.WaitForThreads();
+            BlockForEver();
+
+     
+        }
+
+        static void BlockForEver()
+        {
+            bool complete = false;
+            var t = new Thread(()=>
+                                   {
+                                       bool toggle = false;
+                                       while (!complete) toggle = !toggle;
+                                   }
+                );
+            t.Start();
+            Thread.Sleep(1000);
+            complete = true;
+            t.Join();
+
         }
     }
 }
---
+++ Threading/ThreadingBasics/ThreadingBasics/Rendezvous.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadingBasics
+{
+    internal class Rendezvous
+    {
+        private int a1 = 0;
+        private int b1 = 1;
+        private int a2 = 2;
+        private int b2 = 3;
+        //SemaphoreSlim waitForA = new SemaphoreSlim(5);
+
+        internal void RendezvousWait()
+        {
+           
+        }
+    }
+}
