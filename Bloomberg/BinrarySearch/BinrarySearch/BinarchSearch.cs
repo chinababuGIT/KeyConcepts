ac6235b368f159c89c1d3f378b72828e43bd5f2b
ed <edwardt.tril@gmail.com>
2012-11-19 11:01:12 -0800

    added binary search

---
+++ Bloomberg/BinrarySearch/BinrarySearch/BinarchSearch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics.Contracts;
+
+namespace BinrarySearch
+{
+    internal class BinarchSearch
+    {
+        static bool IsNullOrEmpty(int[] sourceArray) 
+        {
+            return (sourceArray != null) || (sourceArray.Length > 1);
+        }
+
+        public static int BinarySearch(int key, int[] sourceArray, int start, int high)
+        {
+            int indexFound = -1;
+            if(high < start) 
+                return indexFound;
+            else
+            {
+               int midPoint = start + (high-start)/2;
+               if(sourceArray[midPoint]== key)
+               {
+                    indexFound = midPoint;                    
+               }
+               if(sourceArray[midPoint] > key)
+               {
+                    high = midPoint -1;
+                    indexFound = BinarySearch(key, sourceArray, start, high);
+                    //BinrarySearch(
+               }
+
+               if(sourceArray[midPoint] < key)
+               {
+                   start = midPoint +1;
+                   indexFound = BinarySearch(key, sourceArray, start, high);
+               }
+
+            }
+            return indexFound;
+        }
+    }
+}
---
+++ Bloomberg/BinrarySearch/BinrarySearch/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BinrarySearch
+{
+    using BinrarySearch;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarchSearch bSearch = new BinarchSearch();
+            var sourceArray = new int[1]{-1};
+            BinarchSearch.BinarySearch(0,sourceArray,0,0);
+            sourceArray = new int[5]{1,2,3,4,5};
+            BinarchSearch.BinarySearch(1,sourceArray,0,5);
+        }
+    }
+}
