40713c6822621976d43f7ca3b112314ad911bbd8
ed <edwardt.tril@gmail.com>
2013-01-23 00:39:36 -0800

    new interview questions

--- StringAlgorithm/StringRelatedStuff/StringRelatedStuff/Program.cs
+++ StringAlgorithm/StringRelatedStuff/StringRelatedStuff/Program.cs
@@ -3,13 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace StringRelatedStuff
 {
     class Program
     {
+
+        static void BasicStringReverseTest() {
+            var result = StringUtil.ReverseString("a");
+            Debug.Assert(result =="a");
+            result = StringUtil.ReverseString("");
+            Debug.Assert(result == "");
+            result = StringUtil.ReverseString("ab");
+            Debug.Assert(result == "ba");
+            result = StringUtil.ReverseString(null);
+            Debug.Assert(result == null);
+            result = StringUtil.ReverseString("abc");
+            Debug.Assert(result == "cba");
+            result = StringUtil.ReverseString("abcde");
+            Debug.Assert(result=="edcba");
+        }
+
+        static void BasicStringIntersectionTest() {
+            var result = StringUtil.Intersection("", null);
+            Debug.Assert(result == string.Empty);
+            result = StringUtil.Intersection(null, null);
+            Debug.Assert(result == string.Empty);
+            result = StringUtil.Intersection(null, "");
+            Debug.Assert(result == string.Empty);
+            result = StringUtil.Intersection("", "");
+            Debug.Assert(result == string.Empty);
+            result = StringUtil.Intersection("aaa", "aaaa");
+            Debug.Assert(result == "aaa");
+            result = StringUtil.Intersection("aba", "aba");
+            Debug.Assert(result == "aba");
+
+            result = StringUtil.Intersection("abcde", "ab");
+            Debug.Assert(result == "ab");
+
+            result = StringUtil.Intersection("ab", "abcde");
+            Debug.Assert(result == "ab");
+
+            result = StringUtil.Intersection("ab", "de");
+            Debug.Assert(result == string.Empty);
+
+            result = StringUtil.Intersection("ab", "be");
+            Debug.Assert(result == "b");
+
+            result = StringUtil.Intersection("ach", "bceh");
+            Debug.Assert(result == "bh");
+
+
+        }
+
         static void Main(string[] args)
         {
+            //BasicStringReverseTest();
+            BasicStringIntersectionTest();
         }
     }
 }
---
+++ StringAlgorithm/StringRelatedStuff/StringRelatedStuff/StringUtil.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringRelatedStuff
+{
+    public static class StringUtil
+    {
+        public static Tuple<int,int> Find(string source, string substring) 
+        {
+            Tuple<int, int> result = new Tuple<int, int>(0,0);
+
+            return result;
+        }
+
+
+        public static string ReverseString(string source) 
+        { 
+           string result = null;
+           if (String.IsNullOrEmpty(source))
+           {
+               result = source;
+           }
+           else
+           {
+               char[] stringChar = source.ToCharArray();
+               int start = 0;
+               int end = stringChar.Length - 1;
+               while(start < end){
+                   char temp = stringChar[start];
+                   stringChar[start] = stringChar[end];
+                   stringChar[end] = temp;
+                   ++start; --end;
+               }
+               result = new String(stringChar);
+           }
+           return result;
+        }
+
+        public static Tuple<string, string> UncommonString(string leftString, string rightString)
+        {
+            Tuple<string, string> result = new Tuple<string, string>(leftString, rightString);
+
+
+            return result;
+        }
+
+        public static string MergedString(string leftString, string rightString) 
+        {
+            string result = string.Empty;
+
+            return result;
+        }
+
+        //presort the strings
+        public static string Intersection(string leftString, string rightString) 
+        {
+            string result = string.Empty;
+            bool leftStringNullOrEmpty = string.IsNullOrEmpty(leftString);
+            bool rightStringNullOrEmpty = string.IsNullOrEmpty(rightString);
+            if (leftStringNullOrEmpty || rightStringNullOrEmpty)
+            {
+                return result;
+            }
+            else 
+            {
+                int leftStart = 0;
+                int rightStart = 0;
+                int leftStringLength = leftString.Length;
+                int rightStringLength = rightString.Length;
+                char[] leftCharString = leftString.ToCharArray();
+                char[] rightCharString = rightString.ToCharArray();
+
+                int length = Math.Min(leftStringLength,rightStringLength);
+                char[] charResult = new char[length];
+                int i =0;
+
+                while( leftStart < leftStringLength 
+                    && rightStart < rightStringLength)
+                {
+                    if (leftCharString[leftStart] == rightCharString[rightStart])
+                    {
+                        charResult[i] = leftCharString[leftStart];
+                        ++i;
+                        ++leftStart;
+                        ++rightStart;
+                    }
+                    else if (leftCharString[leftStart] < rightCharString[rightStart])
+                    {
+                        ++leftStart;
+                    }
+                    else if (leftCharString[leftStart] > rightCharString[rightStart])
+                    {
+                        ++rightStart;
+                    };
+                };
+
+                if (i > 0)
+                {
+                    if (leftStart < rightStart)
+                    {
+                        for (int j = rightStart; j < rightStringLength; ++j)
+                        {
+                            if (leftCharString[leftStart] == rightCharString[rightStart])
+                            {
+                                charResult[++i] = leftCharString[leftStart];
+                            }
+                            else
+                            {
+                                ;
+                            }
+                        }
+                    };
+
+                    if (leftStart > rightStart)
+                    {
+                        for (int j = rightStart; j < leftStringLength; ++j)
+                        {
+                            if (leftCharString[leftStart] == rightCharString[rightStart])
+                            {
+                                charResult[++i] = rightCharString[leftStart];
+                            }
+                            else
+                            {
+                                ;
+                            }
+                        }
+
+                    };
+                    result = new String(charResult);
+                }
+                else 
+                {
+                    result = string.Empty;
+                };
+            }
+            return result;
+        
+        }
+
+        public static string RemoveSubString(string source, string subString) 
+        {
+            string result = string.Empty;
+
+            return result;        
+        }
+
+
+    }
+}
